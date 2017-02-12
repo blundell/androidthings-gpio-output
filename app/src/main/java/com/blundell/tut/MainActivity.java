5426b66c22193b3fe8b75ed5a8d9ee870d5d56b8
Blundell <blundell.app.dev@gmail.com>
2017-02-12 21:27:19 +0000

    blink the led every second

--- app/src/main/java/com/blundell/tut/MainActivity.java
+++ app/src/main/java/com/blundell/tut/MainActivity.java
@@ -2,18 +2,22 @@ package com.blundell.tut;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
     private static final String GREEN_LED_PIN = "BCM19";
 
     private Gpio bus;
+    private Handler ledToggleHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +37,43 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             throw new IllegalStateException(GREEN_LED_PIN + " bus cannot be configured.", e);
         }
+
+        ledToggleHandler = new Handler(Looper.getMainLooper());
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ledToggleHandler.post(toggleLed);
+    }
+
+    private final Runnable toggleLed = new Runnable() {
+        @Override
+        public void run() {
+            boolean isOn;
+            try {
+                isOn = bus.getValue();
+            } catch (IOException e) {
+                throw new IllegalStateException(GREEN_LED_PIN + " cannot be read.", e);
+            }
+            try {
+                if (isOn) {
+                    bus.setValue(false);
+                } else {
+                    bus.setValue(true);
+                }
+            } catch (IOException e) {
+                throw new IllegalStateException(GREEN_LED_PIN + " cannot be written.", e);
+            }
+            ledToggleHandler.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        ledToggleHandler.removeCallbacks(toggleLed);
+        super.onStop();
+    }
 
     @Override
     protected void onDestroy() {
